5da6ed914cbb9216ba462dd2eeacdf8769167f9b
flaviolzsantos <flaviolzsantos@gmail.com>
2019-07-21 22:31:37 -0300

    commit inicial

---
+++ LuizaLabs.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LuizaLabs.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
